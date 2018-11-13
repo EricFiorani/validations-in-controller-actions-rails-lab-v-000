7cbdb0e40ce04c67e87cfb71bcedb2d9ab6aef54
Eric Fiorani <efiorani305@gmail.com>
2018-11-13 15:37:18 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,9 @@ class PostsController < ApplicationController
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
